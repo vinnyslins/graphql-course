7e949beccc216588c7d81c9af95d45f45e6d0224
Vinnys Lins <vinnysflins@gmail.com>
2020-09-07 01:08:50 -0300

    feat: add bcrypt and mutation to register users

--- backend/resolvers/Mutation/usuario.js
+++ backend/resolvers/Mutation/usuario.js
@@ -1,25 +1,37 @@
+const bcrypt = require('bcrypt-nodejs')
+
 const db = require('../../config/db')
 const { perfil: obterPerfil } = require('../Query/perfil')
 const { usuario: obterUsuario } = require('../Query/usuario')
 
-module.exports = {
+const mutations = {
+  async registrarUsuario(_, { dados }) {
+    return mutations.novoUsuario(_, { dados })
+  },
   async novoUsuario(_, { dados: { perfis: filtroPerfis, ...dados } }) {
+    const salt = bcrypt.genSaltSync()
+    dados.senha = bcrypt.hashSync(dados.senha, salt)
+
     const [id] = await db('usuarios').insert(dados)
 
-    if (filtroPerfis && filtroPerfis.length) {
-      const perfis = await Promise.all(
-        filtroPerfis.map(filtro => obterPerfil(_, { filtro }))
-      )
-
-      await Promise.all(
-        perfis
-          .filter(perfil => perfil)
-          .map(perfil => db('usuarios_perfis').insert({
-            usuario_id: id,
-            perfil_id: perfil.id
-          }))
-      )
+    if (!filtroPerfis || !filtroPerfis.length) {
+      filtroPerfis = [{
+        nome: 'comum'
+      }]
     }
+
+    const perfis = await Promise.all(
+      filtroPerfis.map(filtro => obterPerfil(_, { filtro }))
+    )
+
+    await Promise.all(
+      perfis
+        .filter(perfil => perfil)
+        .map(perfil => db('usuarios_perfis').insert({
+          usuario_id: id,
+          perfil_id: perfil.id
+        }))
+    )
 
     return db('usuarios').where({ id }).first()
   },
@@ -55,7 +67,14 @@ module.exports = {
       }
     }
 
+    if (dados.senha) {
+      const salt = bcrypt.genSaltSync()
+      dados.senha = bcrypt.hashSync(dados.senha, salt)
+    }
+
     await db('usuarios').where({ id: usuario.id }).update(dados)
     return { ...usuario, ...dados }
   }
 }
+
+module.exports = mutations
